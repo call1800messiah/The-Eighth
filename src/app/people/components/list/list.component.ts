6a310d0d778a07753e0c28e7079aa0202a6162b6
call1800messiah <>
2020-06-02 00:59:38 +0200

    Add text filter to people list component

--- src/app/people/components/list/list.component.ts
+++ src/app/people/components/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { combineLatest, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, startWith } from 'rxjs/operators';
 
 import { DataService } from '../../../core/services/data.service';
 import { Person } from 'src/app/core/models/person.model';
@@ -12,15 +14,33 @@ import { Person } from 'src/app/core/models/person.model';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  public people$: Observable<Person[]>
+  filteredPeople$: Observable<Person[]>;
+  textFilter: FormControl;
 
   constructor(
     private data: DataService
   ) {
-    this.people$ = this.data.getPeople();
+    this.textFilter = new FormControl('');
+    this.filteredPeople$ = combineLatest([
+      this.data.getPeople(),
+      this.textFilter.valueChanges.pipe(
+        startWith(''),
+        debounceTime(300),
+        distinctUntilChanged(),
+      ),
+    ]).pipe(
+      map(this.filterPeopleByText),
+    );
   }
 
   ngOnInit(): void {
   }
 
+  private filterPeopleByText(data): Person[] {
+    const [people, text] = data;
+    return people.filter((person) => {
+      return text === ''
+        || person.name.toLowerCase().indexOf(text.toLowerCase()) !== -1;
+    });
+  }
 }
