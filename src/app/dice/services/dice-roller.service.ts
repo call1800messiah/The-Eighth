b9af570841c7a7618cbf95d3a8bf4dd26200e7f7
call1800messiah <>
2020-08-03 23:46:13 +0200

    Add skill check mechanics

--- src/app/dice/services/dice-roller.service.ts
+++ src/app/dice/services/dice-roller.service.ts
@@ -29,6 +29,46 @@ export class DiceRollerService {
   }
 
 
+  rollSkillCheck(
+    first: number,
+    second: number,
+    third: number,
+    skill: number,
+    modifier: number,
+  ): number {
+    console.log('Skill check', first, second, third, skill, modifier);
+    const results = this.rollMany(3, Die.D20);
+    console.log('Rolls', ...results);
+    const effectiveSkill = skill - modifier;
+    let netSkill = effectiveSkill;
+    let attributes;
+
+    if (effectiveSkill < 0) {
+      attributes = [first + effectiveSkill, second + effectiveSkill, third + effectiveSkill];
+      if (attributes[0] > results[0] && attributes[1] > results[1] && attributes[2] > results[2]) {
+        netSkill = 1;
+      }
+    } else {
+      attributes = [first, second, third];
+      for (let i = 0; i < 3; i++) {
+        if (results[i] > attributes[i]) {
+          netSkill -= results[i] - attributes[i];
+        }
+      }
+    }
+
+    if (netSkill > skill) {
+      netSkill = skill;
+    }
+    if (netSkill === 0) {
+      netSkill = 1;
+    }
+    console.log('Remaining', netSkill);
+
+    return netSkill;
+  }
+
+
   private addRollToStats(type: Die, roll: number) {
     if (!this.stats[type]) {
       this.stats[type] = {};
