5f55372916fb0b7c73fe411dce9a34f68bf450af
call1800messiah <>
2022-03-07 22:45:06 +0100

    Add serializing projects for database

--- src/app/projects/services/project.service.ts
+++ src/app/projects/services/project.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
+import firebase from 'firebase';
+import FieldValue = firebase.firestore.FieldValue;
+
 import { ApiService } from '../../core/services/api.service';
 import { AuthService } from '../../core/services/auth.service';
 import { Project } from '../models/project';
@@ -9,6 +12,9 @@ import { DataService } from '../../core/services/data.service';
 import { UtilService } from '../../core/services/util.service';
 import { ProjectRequirement } from '../models/project-requirement';
 import { ProjectMilestone } from '../models/project-milestone';
+import { ProjectDB } from '../models/project.db';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +32,8 @@ export class ProjectService {
     this.user = this.auth.user;
   }
 
+
+
   getProjects(): Observable<Project[]> {
     if (!this.projects$) {
       this.projects$ = new BehaviorSubject<Project[]>([]);
@@ -42,11 +50,76 @@ export class ProjectService {
     return this.projects$;
   }
 
+
   store(project: Partial<Project>, projectId?: string): Promise<boolean> {
-    console.log(projectId, project);
-    return Promise.resolve(true);
-    // return this.data.store(project, ProjectService.collection, projectId);
+    return this.data.store(this.serializeProject(project, projectId), ProjectService.collection, projectId);
   }
+
+
+
+  private serializeProject(project: Partial<Project>, projectID?: string): ProjectDB {
+    const mDesc: Record<string, string | FieldValue> = {};
+    const mReq: Record<string, number | FieldValue> = {};
+    const rCur: Record<string, number | FieldValue> = {};
+    const rReq: Record<string, number | FieldValue> = {};
+    const rSkill: Record<string, string | FieldValue> = {};
+
+    Object.entries(project).forEach(([key, value]) => {
+      const split = key.split('-');
+      if (split[0] === 'mile' && split[2] === 'req') {
+        mReq[split[1]] = value as unknown as number;
+      }
+      if (split[0] === 'mile' && split[2] === 'desc') {
+        mDesc[split[1]] = value as unknown as string;
+      }
+      if (split[0] === 'req' && split[2] === 'req') {
+        rReq[split[1]] = value as unknown as number;
+      }
+      if (split[0] === 'req' && split[2] === 'cur') {
+        rCur[split[1]] = value as unknown as number;
+      }
+      if (split[0] === 'req' && split[2] === 'skill') {
+        rSkill[split[1]] = value as unknown as string;
+      }
+    });
+
+    if (projectID) {
+      this.projects$.pipe(
+        take(1),
+        map((projects) => projects.find((p) => p.id === projectID))
+      ).subscribe((currentProject) => {
+        if (currentProject) {
+          currentProject.milestones.forEach((milestone) => {
+            if (!mDesc[milestone.id]) {
+              mDesc[milestone.id] = FieldValue.delete();
+              mReq[milestone.id] = FieldValue.delete();
+            }
+          });
+          currentProject.requirements.forEach((requirement) => {
+            if (!rSkill[requirement.id]) {
+              rSkill[requirement.id] = FieldValue.delete();
+              rReq[requirement.id] = FieldValue.delete();
+              rCur[requirement.id] = FieldValue.delete();
+            }
+          });
+        }
+      });
+    }
+
+    return {
+      benefit: project.benefit,
+      interval: project.interval,
+      isPrivate: project.isPrivate,
+      mDesc,
+      mReq,
+      name: project.name,
+      owner: project.owner,
+      rCur,
+      rReq,
+      rSkill
+    };
+  }
+
 
   private transformProjects(projects: any[]): Project[] {
     return projects.reduce((all, entry) => {
@@ -67,7 +140,7 @@ export class ProjectService {
       const requirements: ProjectRequirement[] = !projectData.rSkill || !projectData.rCur || !projectData.rReq
         ? []
         : Object.entries(projectData.rSkill).reduce((allR, [id, skill]) => {
-        if (projectData.rCur[id] && projectData.rReq[id]) {
+        if (projectData.rCur[id] !== undefined && projectData.rReq[id]) {
           allR.push({
             id,
             skill,
