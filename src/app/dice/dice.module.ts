81b6e4d7ab8a62c2d56ce11aeac214b494a8ac47
call1800messiah <>
2022-03-21 21:29:56 +0100

    Add hit location die to footer

--- src/app/dice/dice.module.ts
+++ src/app/dice/dice.module.ts
@@ -4,16 +4,19 @@ import { CommonModule } from '@angular/common';
 import { QuickDiceComponent } from './components/quick-dice/quick-dice.component';
 import { DieComponent } from './components/die/die.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HitLocationDieComponent } from './components/hit-location-die/hit-location-die.component';
 
 
 
 @NgModule({
   declarations: [
     DieComponent,
-    QuickDiceComponent
+    HitLocationDieComponent,
+    QuickDiceComponent,
   ],
   exports: [
     DieComponent,
+    HitLocationDieComponent,
     QuickDiceComponent,
   ],
   imports: [
