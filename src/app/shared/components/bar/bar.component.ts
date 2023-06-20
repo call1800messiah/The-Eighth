7d399cabfc792f072e3ff24779dacba6ff7f927c
call1800messiah <32394062+call1800messiah@users.noreply.github.com>
2023-06-20 23:54:20 +0200

    feat: Make attribute bars go above max and below zero

--- src/app/shared/components/bar/bar.component.ts
+++ src/app/shared/components/bar/bar.component.ts
@@ -14,6 +14,7 @@ export class BarComponent implements OnInit {
   @Input() bar: Bar;
   @Output() barClicked = new EventEmitter();
   @ViewChild('barElement', {static: true}) barElement: ElementRef;
+  @ViewChild('extraBar', {static: true}) extraBar: ElementRef;
 
   constructor(
     private popover: PopoverService,
@@ -24,8 +25,21 @@ export class BarComponent implements OnInit {
     this.renderer.setStyle(
       this.barElement.nativeElement,
       'width',
-      `${this.bar.current / this.bar.max * 100}%`,
+      this.bar.current > 0 ? `${this.bar.current / this.bar.max * 100}%` : '0',
     );
+    let extra = 0;
+    if (this.bar.current > this.bar.max) {
+      extra = this.bar.current - this.bar.max;
+    } else if (this.bar.current < 0) {
+      extra = Math.abs(this.bar.current);
+    }
+    if (extra) {
+      this.renderer.setStyle(
+        this.extraBar.nativeElement,
+        'width',
+        `${extra / this.bar.max * 100}%`,
+      );
+    }
   }
 
 
