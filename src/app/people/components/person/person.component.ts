4e5f52f4f44d7b32d7e6099fda9199d1fa239844
call1800messiah <32394062+call1800messiah@users.noreply.github.com>
2023-07-29 12:47:01 +0200

    refactor: Remove unused import

--- src/app/people/components/person/person.component.ts
+++ src/app/people/components/person/person.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { faBars, faTags } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Observable, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
@@ -34,7 +34,6 @@ import { EditTagsComponent } from '../../../shared/components/edit-tags/edit-tag
 })
 export class PersonComponent implements OnInit, OnDestroy {
   faBars = faBars;
-  faTags = faTags;
   infos$: Observable<Map<InfoType, Info[]>>;
   menu: Menu = {
     actions: [
